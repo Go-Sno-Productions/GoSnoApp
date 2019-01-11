429184a78907bd743efb9f992bb4e4b16e289acd
Rokas Jasonas <rokas.jasonas@gmail.com>
2019-01-11 20:46:17 +0200

    Add missing general info

--- app/src/main/java/com/gosno/app/generalinfo/GeneralInfoFragment.kt
+++ app/src/main/java/com/gosno/app/generalinfo/GeneralInfoFragment.kt
@@ -1,5 +1,7 @@
 package com.gosno.app.generalinfo
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.gosno.app.R
 import kotlinx.android.synthetic.main.fragment_general_info.*
+
 
 class GeneralInfoFragment : Fragment() {
     override fun onCreateView(
@@ -18,8 +21,22 @@ class GeneralInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        hotelAvantiTextView.setOnClickListener {  }
-        hotelCimsTextView.setOnClickListener {  }
+        hotelAvantiTextView.setOnClickListener { openUrl("https://goo.gl/maps/RquWscYrhfp") }
+        hotelCimsTextView.setOnClickListener { openUrl("https://goo.gl/maps/1PzeeUGor2S2") }
+        pranasTextView.setOnClickListener { openUrl("https://m.facebook.com/PranasGataveckas") }
+        phoneTextView.setOnClickListener { callPhone() }
+    }
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+    }
+
+    private fun callPhone() {
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:" + getString(R.string.phone_number))
+        startActivity(intent)
     }
 
     companion object {
