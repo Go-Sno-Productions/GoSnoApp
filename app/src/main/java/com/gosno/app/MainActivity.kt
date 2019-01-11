3eecc90cf995987676364b63cf05ceb4d1d93933
Rokas Jasonas <rokas.jasonas@gmail.com>
2019-01-11 19:14:43 +0200

    Add Trace snow button logic

--- app/src/main/java/com/gosno/app/MainActivity.kt
+++ app/src/main/java/com/gosno/app/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.gosno.app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.gosno.app.R.id.traceSnow
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            menuItem.isChecked = true
-            drawerLayout.closeDrawers()
-
-            true
-        }
-
+        setUpNavigationView()
         setUpToolbar()
         setUpFragment()
+    }
+
+    private fun setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            drawerLayout.closeDrawers()
+            when (menuItem.itemId) {
+                traceSnow -> {
+                    openTraceSnow()
+                    false
+                }
+                else -> true
+            }
+        }
+    }
+
+    private fun openTraceSnow() {
+        val launchIntent = packageManager.getLaunchIntentForPackage(TRACE_SNOW_PACKAGE_NAME)
+        if (launchIntent != null) {
+            startActivity(launchIntent)
+        } else {
+            openTraceSnowPlayStore()
+        }
+    }
+
+    private fun openTraceSnowPlayStore() {
+        val playStoreIntent = Intent(Intent.ACTION_VIEW);
+        playStoreIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        playStoreIntent.data = Uri.parse("market://details?id=$TRACE_SNOW_PACKAGE_NAME");
+        startActivity(playStoreIntent)
     }
 
     private fun setUpFragment() {
@@ -37,10 +63,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean =
-        if (item.itemId == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START)
-            true
-        } else {
-            super.onOptionsItemSelected(item)
-        }
+            if (item.itemId == android.R.id.home) {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            } else {
+                super.onOptionsItemSelected(item)
+            }
+
+    companion object {
+        private const val TRACE_SNOW_PACKAGE_NAME = "com.alpinereplay.android"
+    }
 }
