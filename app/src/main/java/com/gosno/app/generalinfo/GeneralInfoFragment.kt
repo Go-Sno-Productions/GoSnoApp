3f05e71605999824fdc869bc1ef25991a4127528
Rokas Jasonas <rokas.jasonas@gmail.com>
2019-01-16 18:25:07 +0200

    Open facebook app from General Info

--- app/src/main/java/com/gosno/app/generalinfo/GeneralInfoFragment.kt
+++ app/src/main/java/com/gosno/app/generalinfo/GeneralInfoFragment.kt
@@ -1,6 +1,7 @@
 package com.gosno.app.generalinfo
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -13,9 +14,9 @@ import kotlinx.android.synthetic.main.fragment_general_info.*
 
 class GeneralInfoFragment : Fragment() {
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_general_info, container, false)
 
 
@@ -23,7 +24,7 @@ class GeneralInfoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         hotelAvantiTextView.setOnClickListener { openUrl("https://goo.gl/maps/RquWscYrhfp") }
         hotelCimsTextView.setOnClickListener { openUrl("https://goo.gl/maps/1PzeeUGor2S2") }
-        pranasTextView.setOnClickListener { openUrl("https://m.facebook.com/PranasGataveckas") }
+        pranasTextView.setOnClickListener { openFacebook("https://m.facebook.com/PranasGataveckas") }
         phoneTextView.setOnClickListener { callPhone() }
     }
 
@@ -33,6 +34,18 @@ class GeneralInfoFragment : Fragment() {
         startActivity(intent)
     }
 
+    private fun openFacebook(url: String) = try {
+        val applicationInfo = context!!.packageManager.getApplicationInfo(FACEBOOK_PACKAGE, 0)
+        val uri = if (applicationInfo.enabled) {
+            Uri.parse(FACEBOOK_ACTION + url)
+        } else {
+            Uri.parse(url)
+        }
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    } catch (ignored: PackageManager.NameNotFoundException) {
+        // Empty
+    }
+
     private fun callPhone() {
         val intent = Intent(Intent.ACTION_DIAL)
         intent.data = Uri.parse("tel:" + getString(R.string.phone_number))
@@ -40,6 +53,9 @@ class GeneralInfoFragment : Fragment() {
     }
 
     companion object {
+        private const val FACEBOOK_PACKAGE = "com.facebook.katana"
+        private const val FACEBOOK_ACTION = "fb://facewebmodal/f?href="
+
         fun newInstance(): GeneralInfoFragment = GeneralInfoFragment()
     }
 }
