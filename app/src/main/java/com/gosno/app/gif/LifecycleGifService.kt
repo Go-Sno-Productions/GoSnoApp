2da730443eea75dc4fd8bb33a7a9251b561a6a20
VaidasKr <vaikrs@telesoftas.com>
2019-01-17 13:45:15 +0200

    Create LifecycleGifService.kt

---
+++ app/src/main/java/com/gosno/app/gif/LifecycleGifService.kt
@@ -0,0 +1,31 @@
+package com.gosno.app.gif
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+import com.gosno.app.R
+
+class LifecycleGifService(private val listener: (gifRes: Int) -> Unit) : LifecycleObserver {
+    private val service = GifService()
+    private var gifRotator: GifRotator? = null
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart() {
+        service.start { bucket ->
+            val rotator = GifRotator(bucket)
+            gifRotator = rotator
+            listener(rotator.nextId())
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStop() {
+        service.cancel()
+    }
+
+    fun requestNewGif() = gifRotator?.nextId() ?: DEFAULT_GIF
+
+    companion object {
+        private const val DEFAULT_GIF = R.drawable.img_default
+    }
+}
