885e6a7d32bdcd2c8ce65bec6f4939acdebc66d7
Rokas Jasonas <rokas.jasonas@gmail.com>
2019-01-17 17:50:59 +0200

    Add onion ski map

--- app/src/main/java/com/gosno/app/piste/PistesFragment.kt
+++ app/src/main/java/com/gosno/app/piste/PistesFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gosno.app.R
+import com.gosno.app.isOnion
 import kotlinx.android.synthetic.main.fragment_pistes.*
 
 class PistesFragment : Fragment() {
@@ -21,17 +22,23 @@ class PistesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        imageView.showImage(getUri(R.drawable.img_skimap))
+        imageView.showImage(getSkiMapUri())
     }
 
-    private fun getUri(resourceId: Int): Uri {
-        return Uri.Builder()
-            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-            .authority(resources.getResourcePackageName(resourceId))
-            .appendPath(resources.getResourceTypeName(resourceId))
-            .appendPath(resources.getResourceEntryName(resourceId))
-            .build()
+    private fun getSkiMapUri() = getUri(getSkiMapResId())
+
+    private fun getSkiMapResId() = if (context?.isOnion() == true) {
+        R.drawable.img_onion_skimap
+    } else {
+        R.drawable.img_skimap
     }
+
+    private fun getUri(resourceId: Int): Uri = Uri.Builder()
+        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+        .authority(resources.getResourcePackageName(resourceId))
+        .appendPath(resources.getResourceTypeName(resourceId))
+        .appendPath(resources.getResourceEntryName(resourceId))
+        .build()
 
     companion object {
         fun newInstance(): PistesFragment = PistesFragment()
