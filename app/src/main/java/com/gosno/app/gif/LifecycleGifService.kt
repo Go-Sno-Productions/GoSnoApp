b75b7efa4ee66cb85a1b4b9fa9e2658b87219a8c
VaidasKr <vaikrs@telesoftas.com>
2019-01-17 16:18:37 +0200

    Update LifecycleGifService.kt to use annonymus lifecycleObserver

--- app/src/main/java/com/gosno/app/gif/LifecycleGifService.kt
+++ app/src/main/java/com/gosno/app/gif/LifecycleGifService.kt
@@ -5,22 +5,26 @@ import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
 import com.gosno.app.R
 
-class LifecycleGifService(private val listener: (gifRes: Int) -> Unit) : LifecycleObserver {
+class LifecycleGifService(private val listener: (gifRes: Int) -> Unit) {
     private val service = GifService()
     private var gifRotator: GifRotator? = null
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
-    fun onStart() {
-        service.start { bucket ->
-            val rotator = GifRotator(bucket)
-            gifRotator = rotator
-            listener(rotator.nextId())
-        }
-    }
+    fun attach(lifecycle: Lifecycle) {
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            fun onStart() {
+                service.start { bucket ->
+                    val rotator = GifRotator(bucket)
+                    gifRotator = rotator
+                    listener(rotator.nextId())
+                }
+            }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-    fun onStop() {
-        service.cancel()
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            fun onStop() {
+                service.cancel()
+            }
+        })
     }
 
     fun requestNewGif() = gifRotator?.nextId() ?: DEFAULT_GIF
