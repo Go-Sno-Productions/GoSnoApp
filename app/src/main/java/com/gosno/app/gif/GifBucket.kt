827990fd6c9d1085cbc4f1b0940a52ec5dff606e
VaidasKr <vaikrs@telesoftas.com>
2019-01-17 13:13:39 +0200

    Refactor GifBucket.kt to use List instead of array

--- app/src/main/java/com/gosno/app/gif/GifBucket.kt
+++ app/src/main/java/com/gosno/app/gif/GifBucket.kt
@@ -1,25 +1,12 @@
 package com.gosno.app.gif
 
-data class GifBucket(val id: Int, val gifResources: IntArray) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as GifBucket
-
-        return id == other.id && gifResources.contentEquals(other.gifResources)
-    }
-
-    override fun hashCode(): Int {
-        return 31 * id + gifResources.contentHashCode()
-    }
-
+data class GifBucket(val id: Int, val gifResources: List<Int>) {
     companion object {
         // TODO provide resources for gif buckets
 
-        fun waiting() = GifBucket(0, intArrayOf())
-        fun party() = GifBucket(1, intArrayOf())
-        fun skiing() = GifBucket(2, intArrayOf())
-        fun bye() = GifBucket(3, intArrayOf())
+        fun waiting() = GifBucket(0, listOf())
+        fun party() = GifBucket(1, listOf())
+        fun skiing() = GifBucket(2, listOf())
+        fun bye() = GifBucket(3, listOf())
     }
 }
