e1186f953efcdee5189d3fef2ffd6a12fa70a435
Rokas Jasonas <rokas.jasonas@gmail.com>
2019-01-17 18:08:26 +0200

    Add Antanas to About screen

--- app/src/main/java/com/gosno/app/about/AboutFragment.kt
+++ app/src/main/java/com/gosno/app/about/AboutFragment.kt
@@ -17,19 +17,63 @@ import nl.dionsegijn.konfetti.models.Shape
 import nl.dionsegijn.konfetti.models.Size
 
 class AboutFragment : Fragment() {
+    private var clickCount = 0
+    private var firstClickTime = 0L
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_about, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpImageViews()
+        setUpOnionViews()
         view.post { startKonfetti() }
-        rokasImageView.setOnClickListener {
-            it.context.setIsOnion(!it.context.isOnion())
-            activity!!.recreate()
+        vaidasImageView.setOnClickListener {
+            click()
+        }
+    }
+
+    private fun setUpOnionViews() {
+        if (context!!.isOnion()) {
+            antanasImageView.visibility = View.VISIBLE
+            antanasTextView.visibility = View.VISIBLE
+            loadCircleImageView(R.drawable.antanas, antanasImageView)
+        }
+    }
+
+    private fun click() {
+        if (firstClickTime == 0L) {
+            firstClickTime = System.currentTimeMillis()
+        }
+
+        if (isClickingTimeExceeded()) {
+            resetClicks()
+        }
+
+        clickCount++
+
+        if (isClickingSuccessful()) {
+            resetClicks()
+            toggleOnionMode()
+        }
+    }
+
+    private fun isClickingSuccessful() = clickCount == CLICKS_TO_TOGGLE_ONION_MODE
+
+    private fun isClickingTimeExceeded() = System.currentTimeMillis() - firstClickTime > CLICK_TIMEOUT
+
+    private fun resetClicks() {
+        clickCount = 0
+        firstClickTime = 0
+    }
+
+    private fun toggleOnionMode() {
+        activity?.apply {
+            setIsOnion(!isOnion())
+            recreate()
         }
     }
 
@@ -40,25 +84,28 @@ class AboutFragment : Fragment() {
 
     private fun loadCircleImageView(drawableResId: Int, imageView: ImageView) {
         Glide.with(context!!)
-            .load(drawableResId)
-            .apply(RequestOptions.circleCropTransform())
-            .into(imageView)
+                .load(drawableResId)
+                .apply(RequestOptions.circleCropTransform())
+                .into(imageView)
     }
 
     private fun startKonfetti() = konfetti?.apply {
         konfetti.build()
-            .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
-            .setDirection(0.0, 359.0)
-            .setSpeed(1f, 5f)
-            .setFadeOutEnabled(true)
-            .setTimeToLive(2000L)
-            .addShapes(Shape.RECT, Shape.CIRCLE)
-            .addSizes(Size(12))
-            .setPosition(-50f, konfetti.width + 50f, -50f, -50f)
-            .stream(300, 5000L)
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(Size(12))
+                .setPosition(-50f, konfetti.width + 50f, -50f, -50f)
+                .stream(300, 5000L)
     }
 
     companion object {
+        private const val CLICK_TIMEOUT = 5000L
+        private const val CLICKS_TO_TOGGLE_ONION_MODE = 7
+
         fun newInstance(): AboutFragment = AboutFragment()
     }
 }
